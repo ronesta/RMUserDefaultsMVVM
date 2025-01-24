b2cc10389addc97dace7217dea1e687a25709da1
ronesta <gvozzzd123@gmail.com>
2025-01-24 21:33:06 +0300

    Initial commit

---
+++ RMUserDefaultsMVVM/RMUserDefaultsMVVM/Character.swift
@@ -0,0 +1,25 @@
+//
+//  Character.swift
+//  RMUserDefaultsMVVM
+//
+//  Created by Ибрагим Габибли on 24.01.2025.
+//
+
+import Foundation
+
+struct PostCharacters: Codable {
+    let results: [Character]
+}
+
+struct Character: Codable {
+    let name: String
+    let status: String
+    let species: String
+    let gender: String
+    let location: Location
+    let image: String
+}
+
+struct Location: Codable {
+    let name: String
+}
